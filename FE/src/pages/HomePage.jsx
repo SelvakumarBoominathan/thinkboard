728e6dd7606253acef4d8dbccc0f2762fe7990e3
Selvakumar Boominathan <selvans2k@gmail.com>
2025-11-19 20:21:40 +0530

    implemented useEFfect to fetch data using axios in Home page in FE

--- FE/src/pages/HomePage.jsx
+++ FE/src/pages/HomePage.jsx
@@ -1,9 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar.jsx";
 import RateLimitedUI from "../components/RateLimitedUI.jsx";
+import axios from "axios";
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes");
+        console.log(res.data);
+        // setLoading(false);
+      } catch (error) {
+        console.log("Error fetching notes: ", error);
+      }
+    };
+    getNotes();
+  }, []);
 
   return (
     <div className="min-h-screen">
