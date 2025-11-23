a7eab1dc0a2a9c8b10408833f9f26b441fb5fd7f
Selvakumar Boominathan <selvans2k@gmail.com>
2025-11-23 15:02:31 +0530

    Final IP

--- BE/src/server.js
+++ BE/src/server.js
@@ -1,6 +1,7 @@
 import "./config/loadEnv.js";
 import cors from "cors";
 import express from "express";
+import path from "path"; // to make common domain for both FE and BE in production. inbuild in nodejs
 
 import { connectDB } from "./config/db.js";
 import notesRoutes from "./routes/notesRoutes.js";
@@ -8,22 +9,33 @@ import rateLimiter from "./middleware/rateLimiter.js";
 
 const app = express();
 const PORT = process.env.PORT;
+const __dirName = path.resolve(); // this will give the root directory of the project for BE
 
 //middleware
 app.use(express.json()); // this middleware allows us to parse JSON bodies
 
 //CORS Middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173", // Adjust this to your frontend's origin
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // Adjust this to your frontend's origin
+    })
+  );
+}
 
 // Rate Limiter Middleware
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
+// Serve static files in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirName, "../FE/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirName, "../FE", "dist", "index.html"));
+  });
+}
 // Production grade update - connect DB first and then start server
 connectDB().then(() => {
   app.listen(PORT, () => {
